85a38e981f5116d217ee12c69d30b4e3b8801a34
ramirez <prlara2000@gmail.com>
2023-03-06 22:59:26 -0600

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import logo from '../assets/snapLogo.jpg';
+import cartLogo from '../assets/cart.png';
+
+function Header({ items }) {
+  return (
+    <header id='header'>
+      <div id='leftInfo'>
+        <div id='snapLogo'>
+          <img id='imgLogo' src={logo} alt='snap logo'></img>
+          <h1 id='snap'>Snap</h1>
+        </div>
+        <div className='categories'>
+          <h3 className='women'>Women</h3>
+          <h3 className='men'>Men</h3>
+          <h3 className='kids'>Kids</h3>
+        </div>
+      </div>
+      <div id='rightInfo'>
+        <p id='itemsInCart'>{items}</p>
+        <img id='cart' src={cartLogo} alt='cart' />
+      </div>
+    </header>
+  );
+}
+export default Header;
